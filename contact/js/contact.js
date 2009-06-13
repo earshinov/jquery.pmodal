a6c5e13c74e6b2fbd56677cd1ff99bc2fa568f79
Eugene Arshinov <earshinov@gmail.com>
2009-06-13 14:22:41 +0400

    Removed AJAX from 'contact' page as I don't want to setup a web-server for testing.

--- contact/js/contact.js
+++ contact/js/contact.js
@@ -15,18 +15,51 @@
 $(document).ready(function () {
 	$('#contactForm input.contact, #contactForm a.contact').click(function (e) {
 		e.preventDefault();
-		// load the contact form using ajax
-		$.get("data/contact.php", function(data){
-			// create a modal dialog with the data
-			$(data).modal({
-				close: false,
-				position: ["15%",],
-				overlayId: 'contact-overlay',
-				containerId: 'contact-container',
-				onOpen: contact.open,
-				onShow: contact.show,
-				onClose: contact.close
-			});
+
+		data=" \
+<div style='display:none'> \
+<a href='#' title='Close' class='modalCloseX simplemodal-close'>x</a> \
+<div class='contact-top'></div> \
+<div class='contact-content'> \
+<h1 class='contact-title'>Send us a message:</h1> \
+<div class='contact-loading' style='display:none'></div> \
+<div class='contact-message' style='display:none'></div> \
+<form action='#' style='display:none'> \
+\
+<label for='contact-name'>*Name:</label> \
+<input type='text' id='contact-name' class='contact-input' name='name' tabindex='1001' /> \
+<label for='contact-email'>*Email:</label> \
+<input type='text' id='contact-email' class='contact-input' name='email' tabindex='1002' /> \
+\
+<label for='contact-subject'>Subject:</label> \
+<input type='text' id='contact-subject' class='contact-input' name='subject' value='' tabindex='1003' /> \
+\
+<label for='contact-message'>*Message:</label> \
+<textarea id='contact-message' class='contact-input' name='message' cols='40' rows='4' tabindex='1004'></textarea> \
+<br/> \
+\
+<label>&nbsp;</label> \
+<input type='checkbox' id='contact-cc' name='cc' value='1' tabindex='1005' /> <span class='contact-cc'>Send me a copy</span> \
+<br/> \
+\
+<label>&nbsp;</label> \
+<button type='submit' class='contact-send contact-button' tabindex='1006'>Send</button> \
+<button type='submit' class='contact-cancel contact-button simplemodal-close' tabindex='1007'>Cancel</button> \
+<br/> \
+\
+</form> \
+</div> \
+<div class='contact-bottom'><a href='http://www.ericmmartin.com/projects/simplemodal/'>Powered by SimpleModal</a></div> \
+</div>";
+
+		$(data).modal({
+			close: false,
+			position: ["15%",],
+			overlayId: 'contact-overlay',
+			containerId: 'contact-container',
+			onOpen: contact.open,
+			onShow: contact.show,
+			onClose: contact.close
 		});
 	});
 
@@ -143,7 +176,7 @@ var contact = {
 						height: '30px'
 					}, contact.showError);
 				}
-				
+
 			}
 		});
 	},
@@ -196,7 +229,7 @@ var contact = {
 	validateEmail: function (email) {
 		var at = email.lastIndexOf("@");
 
-		// Make sure the at (@) sybmol exists and  
+		// Make sure the at (@) sybmol exists and
 		// it is not the first or last character
 		if (at < 1 || (at + 1) === email.length)
 			return false;
@@ -228,7 +261,7 @@ var contact = {
 
 		// Make sure domain contains only valid characters and at least one period
 		if (!/^[-a-zA-Z0-9\.]*$/.test(domain) || domain.indexOf(".") === -1)
-			return false;	
+			return false;
 
 		return true;
 	},
